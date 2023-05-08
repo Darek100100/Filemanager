da80cf2a1ef75ab5641aa95b706de4620cdc62fe
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-08 23:48:49 +0200

    added method size sort directory files

--- Filemanager/src/main/java/org/example/Filemanager.java
+++ Filemanager/src/main/java/org/example/Filemanager.java
@@ -1,19 +1,34 @@
 package org.example;
 
 import java.io.File;
-import java.rmi.server.UnicastRemoteObject;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import static sun.util.locale.LocaleMatcher.filter;
 
 public class Filemanager {
-    public void listFilesManager(String directiory){
-        Stream.of(Objects.requireNonNull(new File(directiory).listFiles()))
+    public void listFilesManager(String directory) {
+        System.out.println("te pliki masz w folderze: ");
+
+        Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
                 .filter(File::isFile)  //filtr by zostaly tylko pliki
                 .map(File::getName)  //by dostawac wszystkie nazwy plików
                 .forEach(System.out::println);
-
-
     }
+
+           public void sortFilesInDirectory(String directory){
+            Stream.of(Objects.requireNonNull(new File(directory).listFiles()))
+                    .filter(File::isFile)
+                    .sorted((o1, o2) -> Long.compare(o2.length(), o1.length()))
+                    .forEach(file -> {
+                        System.out.println(file.getName());
+                        System.out.println(file.length());
+                        System.out.println();
+                    });
+
+        }
 }
+
+
+
+
